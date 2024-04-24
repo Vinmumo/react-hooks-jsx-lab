fe00104b05e3b2b28af37568082c9baa4142944b
vincent <vinnymummo@gmail.com>
2024-04-24 10:43:30 +0300

    commit

--- src/components/Home.js
+++ src/components/Home.js
@@ -3,7 +3,9 @@ import { name, city } from "../data/data.js";
 
 function Home() {
   // update the JSX being returned!
-  return <div>Home</div>;
+  return (
+  <div id="home" style={{color: "firebrick"}}>{name} is a Web Developer from {city}</div>
+  )
 }
 
 export default Home;
